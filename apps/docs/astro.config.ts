22eb3c64e867efc0d53e35e57068692fa2f5e550
francyfox <7info7web@gmail.com>
2025-10-31 17:13:09 +0500

    doc: sitemap

--- apps/docs/astro.config.ts
+++ apps/docs/astro.config.ts
@@ -6,13 +6,14 @@ import starlightChangelogs, {
 } from "starlight-changelogs";
 import UnoCSS from "unocss/astro";
 
+const googleAnalyticsId = "GTM-PWXSZLDZ";
 // https://astro.build/config
 export default defineConfig({
 	// i18n: {
 	// 	locales: ["en", "ru"],
 	// 	defaultLocale: "en",
 	// },
-	// site: "bunsqstat.shalotts.site",
+	site: "https://bunsqstat.shalotts.site",
 	integrations: [
 		UnoCSS(),
 		starlight({
@@ -23,6 +24,28 @@ export default defineConfig({
 			components: {
 				MarkdownContent: "./src/components/MarkdownContent.astro",
 			},
+			head: [
+				{
+					tag: "noscript",
+					content: `
+					<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=${googleAnalyticsId}"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+					
+					`,
+				},
+				{
+					tag: "script",
+					content: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${googleAnalyticsId}');
+          `,
+				},
+			],
 			locales: {
 				root: {
 					lang: "en",
